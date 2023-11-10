a1c1dce7dd7a980eaeb78330d1ac0cc717218947
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-11-10 23:30:54 +0530

    arrows added prev and next

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(`mongodb+srv://ngawangg:applepie@cluster0.7h0rl9g.mongodb.net/t
 
 
 const postSchema = {
+    postNumber: Number,
     title: String ,
     questionText:String ,
     answer:String,
@@ -30,20 +31,49 @@ const postSchema = {
 const Post = mongoose.model("Post", postSchema );
 
 
-app.post("/",function(req,res){
-    const post = new Post({
-        title : req.body.title,
-        question : req.body.questionText ,
-        answer : req.body.answer, 
-        imageUpload : req.body.imageUpload,
-        url : req.body.url,
-        submitTime: req.body.submitTime,
-        subject: req.body.subject,
-        topic:req.body.topic ,
-    });
-    post.save()
-    res.redirect('/check')
+// app.post("/",function(req,res){
+//     const post = new Post({
+    
+//         title : req.body.title,
+//         question : req.body.questionText ,
+//         answer : req.body.answer, 
+//         imageUpload : req.body.imageUpload,
+//         url : req.body.url,
+//         submitTime: req.body.submitTime,
+//         subject: req.body.subject,
+//         topic:req.body.topic ,
+//     });
+//     post.save()
+//     res.redirect('/check')
+// });
+
+app.post("/", async function(req, res) {
+    try {
+        // Count the existing posts
+        const postCount = await Post.countDocuments();
+
+        // Create a new post with the incremented post number
+        const post = new Post({
+            postNumber: postCount + 1,
+            title: req.body.title,
+            question: req.body.questionText,
+            answer: req.body.answer,
+            imageUpload: req.body.imageUpload,
+            url: req.body.url,
+            submitTime: req.body.submitTime,
+            subject: req.body.subject,
+            topic: req.body.topic,
+        });
+
+        // Save the new post
+        await post.save();
+        res.redirect('/check');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
 
 
 app.get("/" , function(req,res){
@@ -51,9 +81,14 @@ app.get("/" , function(req,res){
         res.render("home",{
          posts : posts,
         })
-        console.log(posts.length)
-      
      })
+})
+
+
+app.get('/home', (req,res)=>{
+    res.render('home', {
+        posts: [foundPost[0]], // Pass the found post as an array to match your rendering logic
+      });
 })
 
 
@@ -79,28 +114,63 @@ app.post('/admin',(req,res)=>{
 
 
 
-app.get('/api/posts/next/:id', async (req, res) => {
-    const postId = req.params.id;
-  
-    try {
-      // Use findById to find a document by its _id
-      const foundPost = await Post.findById(postId);
-  
-      if (foundPost) {
-        // Render the 'home' page with the specific post
-        res.render('home', {
-          posts: [foundPost], // Pass the found post as an array to match your rendering logic
-        });
-      } else {
-        console.log(`Post with id ${postId} not found`);
-        res.status(404).json({ message: `Post with id ${postId} not found` });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
 
+// app.get('/api/posts/next/:postNumber', async (req, res) => {
+//     const postNumber = req.params.postNumber;
+
+   
+//     try {
+//         const postCount = await Post.countDocuments();
+//         // Use findOne to find a document by its postNumber
+//         if(postNumber === 0){
+//             postNumber = postCount;
+//         }
+//         const foundPost = await Post.findOne({ postNumber: postNumber });
+
+//         if (foundPost) {
+//             // Render the 'home' page with the specific post
+//             res.render('home', {
+//                 posts: [foundPost], // Pass the found post as an array to match your rendering logic
+//             });
+//         } else {
+//             console.log(`Post with postNumber ${postNumber} not found`);
+//             res.status(404).json({ message: `Post with postNumber ${postNumber} not found` });
+//         }
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: 'Internal Server Error' });
+//     }
+// });
+
+
+app.get('/api/posts/next/:postNumber', async (req, res) => {
+    let postNumber = parseInt(req.params.postNumber);
+
+    try {
+        const postCount = await Post.countDocuments();
+
+        if (postNumber === 0) {
+            postNumber = postCount;
+        } else if (postNumber > postCount) {
+            postNumber = 1;
+        }
+
+        const foundPost = await Post.findOne({ postNumber: postNumber });
+
+        if (foundPost) {
+            // Render the 'home' page with the specific post
+            res.render('home', {
+                posts: [foundPost],
+            });
+        } else {
+            console.log(`Post with postNumber ${postNumber} not found`);
+            res.status(404).json({ message: `Post with postNumber ${postNumber} not found` });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 
 
 app.listen(port , () => {
